22bc62cf188e920d08adefba947819967cfa2b97
Kaden Klein <kadenkleinonline@gmail.com>
2022-01-26 22:37:23 -0700

    added image pathing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
   }
